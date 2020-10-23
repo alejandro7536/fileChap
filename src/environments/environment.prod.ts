84f368e395064a67e50e1c812b1e6602b36ccde1
Alejandro Alas <alejandroalas7536@gmail.com>
2020-10-23 14:07:43 -0600

    fileChap v1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyByoLmks7F4hl4bNHcgZsafC1JyKmVFgVw',
+    authDomain: 'filechap-4038b.firebaseapp.com',
+    databaseURL: 'https://filechap-4038b.firebaseio.com',
+    projectId: 'filechap-4038b',
+    storageBucket: 'filechap-4038b.appspot.com',
+    messagingSenderId: '180210137809',
+    appId: '1:180210137809:web:377d1de3e181d1399ea3f0',
+    measurementId: 'G-FRSDXCCYKN'
+  }
 };
